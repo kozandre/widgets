60cf695a9d0440bd49656fc4a5553e9eddcbbe46
Andrey <an.kozimirov@gmail.com>
2025-07-10 12:26:10 +0300

    feat: add PieChart

---
+++ src/components/PieChart/PieChart.jsx
@@ -0,0 +1,34 @@
+import {Pie} from "@nivo/pie";
+import {mockPieData} from "src/data/mockPieData";
+
+export const PieChart = ({ width = 620, height = 300 }) => {
+  return (
+    <Pie
+      data={mockPieData}
+      width={width}
+      height={height}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.6}
+      cornerRadius={2}
+      activeOuterRadiusOffset={8}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#333333"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: 'color' }}
+      enableArcLabels={false}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
+      legends={[
+        {
+          anchor: 'bottom',
+          direction: 'row',
+          translateY: 56,
+          itemWidth: 100,
+          itemHeight: 18,
+          symbolShape: 'circle'
+        }
+      ]}
+    />
+    )
+}
---
+++ src/data/mockPieData.js
@@ -0,0 +1,32 @@
+export const mockPieData = [
+  {
+    "id": "css",
+    "label": "css",
+    "value": 259,
+    "color": "hsl(115, 70%, 50%)"
+  },
+  {
+    "id": "c",
+    "label": "c",
+    "value": 442,
+    "color": "hsl(200, 70%, 50%)"
+  },
+  {
+    "id": "elixir",
+    "label": "elixir",
+    "value": 322,
+    "color": "hsl(228, 70%, 50%)"
+  },
+  {
+    "id": "go",
+    "label": "go",
+    "value": 130,
+    "color": "hsl(75, 70%, 50%)"
+  },
+  {
+    "id": "python",
+    "label": "python",
+    "value": 325,
+    "color": "hsl(283, 70%, 50%)"
+  }
+]
