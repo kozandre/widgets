41536ab5b6d70da5db8e679a757ea2418fd09c39
Andrey <an.kozimirov@gmail.com>
2025-07-15 13:52:41 +0300

    refactor: delete unnecessary graphics

--- src/pages/dashboard/WidgetRenderer/WidgetRenderer.jsx
+++ src/pages/dashboard/WidgetRenderer/WidgetRenderer.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-import LinePlotCustom from 'components/LinePlotCustom/LinePlotCustom';
-import BarChartCustom from 'components/BarChartCustom/BarChartCustom';
 import BarChart from 'components/BarChart/BarChart';
 import PieChart from 'components/PieChart/PieChart';
 import LineChart from "components/LineChart/LineChart";
 import GaugeChart from "components/GaugeChart/GaugeChart";
 
-const diagramData = [
-  { name: 'Jan', value: 40 },
-  { name: 'Feb', value: 35 },
-  { name: 'Mar', value: 4 },
-  { name: 'Apr', value: 28 },
-  { name: 'May', value: 15 },
-];
 
 const WidgetRenderer = ({ type, config }) => {
   switch (type) {
@@ -25,10 +16,6 @@ const WidgetRenderer = ({ type, config }) => {
       return <PieChart config={config} />;
     case 'BarChart':
       return <BarChart {...config} />;
-    case 'LinePlotCustom':
-      return <LinePlotCustom data={diagramData} />;
-    case 'BarChartCustom':
-      return <BarChartCustom />;
     default:
       return <Typography>Unknown Widget</Typography>;
   }
