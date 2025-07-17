cafb70cc80574c70b244b037f146efec8d81da9c
Andrey <an.kozimirov@gmail.com>
2025-07-17 13:29:42 +0300

    fix: fix scrollbar for adding widgets page

--- src/pages/home/HomePage/HomePage.jsx
+++ src/pages/home/HomePage/HomePage.jsx
@@ -3,27 +3,25 @@ import { Box, Card, CardContent, Typography, Button } from '@mui/material';
 import Editor from 'pages/dashboard/Editor/Editor';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import './styles.css';
+
 const HomePage = () => {
   const [showEditor, setShowEditor] = useState(false);
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <Box sx={{ p: 4 }}>
-        {!showEditor ? (
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '80vh',
-            }}
-          >
-            <Card sx={{ maxWidth: 400, padding: 2, textAlign: 'center' }}>
+      <Box className="box">
+        <Box className="box-wrapper" sx={{display: !showEditor ? 'flex' : 'block' }}
+        >
+          {!showEditor ? (
+
+            <Card className="card-wrapper" >
               <CardContent>
                 <Typography variant="h4" gutterBottom>
                   Добро пожаловать
                 </Typography>
-                <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
+                <Typography variant="body1" color="text.secondary" sx={{ mb: 2}}>
                   Нажмите ниже, чтобы начать редактирование
                 </Typography>
                 <Button variant="contained" onClick={() => setShowEditor(true)}>
@@ -31,10 +29,11 @@ const HomePage = () => {
                 </Button>
               </CardContent>
             </Card>
-          </Box>
-        ) : (
-          <Editor />
-        )}
+
+          ) : (
+            <Editor />
+          )}
+        </Box>
       </Box>
     </DndProvider>
   );
